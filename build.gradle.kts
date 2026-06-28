798ad20f4ed8ad3a98742b4ac277762d5847cc49
Braj-Makanjee <braj@Braj-Makanjees-Mac-Studio.local>
2026-06-28 18:39:19 +1000

    fix: AGP 8.5.2 + Kotlin 1.9.24 compatible with Gradle 8.9, bump compose compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
 plugins {
- id("com.android.application") version "8.0.2" apply false
- id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 buildscript {
- repositories {
+    repositories {
         google()
         mavenCentral()
- }
- dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2")
- }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+    }
 }
