375cf269a4656ed8f6df6ef6e788a1aab8447403
Braj-Makanjee <braj@Braj-Makanjees-Mac-Studio.local>
2026-07-16 20:00:11 +1000

    ci: use absolute GITHUB_WORKSPACE path for keystore (Gradle signing resolves relative to app/ module)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    // Ktlint plugin for code formatting and linting
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
 }
 
 buildscript {
